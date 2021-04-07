50d8fb3de0008b9ec17ef62c21ea410b5c41674e
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-04-07 19:24:05 +0100

    add console dir

---
+++ console/bumper_ci_service.ts
@@ -0,0 +1,10 @@
+import { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.0.1/ci/bumper_service.ts";
+import { preReleaseFiles } from "./bumper_ci_service_files.ts";
+
+const b = new BumperService("moogle", Deno.args);
+
+if (b.isForPreRelease()) {
+  b.bump(preReleaseFiles);
+} else {
+  //b.bump(bumperFiles);
+}
